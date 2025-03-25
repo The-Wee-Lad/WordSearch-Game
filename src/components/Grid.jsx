995646a269cd668b5a8d564f99986bf14ac6e78c
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-03-25 20:05:43 +0530

    improve touch

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -86,7 +86,7 @@ function Grid({content, solution, setSolution, setSolvedStrings}){
                     }
                     onMouseDown={(event)=>{startDrag(event,index)}}
                     onTouchStart={(event)=>{startDrag(event,index)}}
-                    onTouchMove={(event) => {dragging(event,index)}}
+                    onTouchMove={(event) => {event.preventDefault();;dragging(event,index)}}
                     
                 >
                 {content.letters[index]}
