0721fd12dc2902429b70da450addaa01120d6659
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-03-27 09:44:18 +0530

    add buy me a coffee link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
             <h1 className="text-4xl text-[#71797E] font-bold ml-1">
                 Wordsearch
             </h1>
-            <div className="h-10 ml-auto hover:bg-pink-400 hover:text-amber-200 text-xl max-sm:text-[0.8rem] text-amber-100 bg-pink-300 rounded-4xl pr-1.5 pl-1.5 font-bold flex justify-center items-center">Donate Me</div>
+            <a className="ml-auto" href="https://buymeacoffee.com/theweelad" target="blank"><div className="h-10 ml-auto hover:bg-pink-400 hover:text-amber-200 text-xl max-sm:text-[0.8rem] text-amber-100 bg-pink-300 rounded-4xl pr-1.5 pl-1.5 font-bold flex justify-center items-center">Donate Me</div></a>
         </div>
     );
 }
